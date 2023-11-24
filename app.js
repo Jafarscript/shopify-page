a9a57af22f57a2e08a9042947f1e16c179dda03d
Jafarscript <lihammedjafar@gmail.com>
2023-11-24 21:52:42 +0100

    Refactor my code

--- app.js
+++ app.js
@@ -1,132 +1,93 @@
 function handleToggle(button, dropdown, items, openClass) {
-  button.addEventListener("click", function () {
-    let isExpanded = button.getAttribute("aria-expanded") === "true";
-
-
-    dropdown.classList.toggle(openClass);
-
-    if (isExpanded) {
-      button.setAttribute("aria-expanded", "false");
-      button.focus();
-    } else {
-      button.setAttribute("aria-expanded", "true");
-      items[0].focus();
-    }
-  });
-
-  // Add event listener for the Escape key
-  document.addEventListener("keydown", function (event) {
-    if (dropdown.classList.contains(openClass)) {
-      if (event.key === "Escape") {
-        button.setAttribute("aria-expanded", "false");
+    button.addEventListener("click", toggleDropdown);
+    document.addEventListener("keydown", handleKeydown);
+    document.addEventListener("click", closeDropdownOutside);
+  
+    function toggleDropdown() {
+      const isExpanded = button.getAttribute("aria-expanded") === "true";
+      dropdown.classList.toggle(openClass);
+  
+      if (isExpanded) {
+        closeDropdown();
         button.focus();
-        dropdown.classList.remove(openClass);
-      } else if (event.key === "ArrowDown" || event.key === "ArrowRight") {
-        focusNextItem(items);
-      } else if (event.key === "ArrowUp" || event.key === "ArrowLeft") {
-        focusPreviousItem(items);
+      } else {
+        button.setAttribute("aria-expanded", "true");
+        focusFirstItem();
       }
     }
-  });
-
-  // Close the menu when clicking outside of it
-  document.addEventListener("click", function (event) {
-    if (!button.contains(event.target) && !dropdown.contains(event.target)) {
+  
+    function handleKeydown(event) {
+      if (dropdown.classList.contains(openClass)) {
+        switch (event.key) {
+          case "Escape":
+            closeDropdown();
+            button.focus();
+            break;
+          case "ArrowDown":
+          case "ArrowRight":
+            focusNextItem(items);
+            break;
+          case "ArrowUp":
+          case "ArrowLeft":
+            focusPreviousItem(items);
+            break;
+        }
+      }
+    }
+  
+    function closeDropdownOutside(event) {
+      if (!button.contains(event.target) && !dropdown.contains(event.target)) {
+        closeDropdown();
+      }
+    }
+  
+    function closeDropdown() {
       button.setAttribute("aria-expanded", "false");
       dropdown.classList.remove(openClass);
     }
-  });
-
-  function focusNextItem(items) {
-    const focusedItem = document.activeElement;
-    const index = Array.from(items).indexOf(focusedItem);
-
-    if (index < items.length - 1) {
-      items[index + 1].focus();
-    } else {
+  
+    function focusNextItem(items) {
+      const index = getNextIndex(items);
+      items[index].focus();
+    }
+  
+    function focusPreviousItem(items) {
+      const index = getPreviousIndex(items);
+      items[index].focus();
+    }
+  
+    function getNextIndex(items) {
+      const focusedItem = document.activeElement;
+      const index = Array.from(items).indexOf(focusedItem);
+      return index < items.length - 1 ? index + 1 : 0;
+    }
+  
+    function getPreviousIndex(items) {
+      const focusedItem = document.activeElement;
+      const index = Array.from(items).indexOf(focusedItem);
+      return index > 0 ? index - 1 : items.length - 1;
+    }
+  
+    function focusFirstItem() {
       items[0].focus();
     }
   }
-
-  function focusPreviousItem(items) {
-    const focusedItem = document.activeElement;
-    const index = Array.from(items).indexOf(focusedItem);
-
-    if (index > 0) {
-      items[index - 1].focus();
-    } else {
-      items[items.length - 1].focus();
-    }
-  }
-}
-
-// Usage for the bell element
-let bell = document.querySelector(".notifyButton");
-let bellDrop = document.querySelector(".drop");
-const bellItems = bellDrop.querySelectorAll('[role="menuitem"]');
-handleToggle(bell, bellDrop, bellItems, "dropOpen");
-
-// Usage for the info element
-let info = document.querySelector(".storename");
-let infoTab = document.querySelector(".storeTab");
-const infoItems = infoTab.querySelectorAll('[role="menuitem"]');
-handleToggle(info, infoTab, infoItems, "tabOpen");
-
-
-// // Usage for the Setup element
-
-
-let setupButton = document.getElementById('setupButton');
-let setupStep = document.getElementById('setupStep');
-
-
-
-setupButton.addEventListener('click', function () {
-    let isShown = setupButton.getAttribute("aria-expanded") === "true";
-    
-    const stepItems = setupStep.querySelectorAll('[role="menuitem"]');
-
-    setupStep.classList.toggle('show');
-
-    if (isShown) {
-        setupButton.setAttribute("aria-expanded", "false");
-        setupButton.focus();
-      } else {
-        setupButton.setAttribute("aria-expanded", "true");
-        stepItems[0].focus();
-        // console.log(stepItems[0].focus())
-      }
-    // Toggle rotation class for the SVG inside setupButton
-    let arrowIcon = setupButton.querySelector('svg');
-    arrowIcon.classList.toggle('rotate180');
-
-    document.addEventListener('keydown', function (event) {
-        if (event.key === 'Escape' && setupStep.classList.contains('show')) {
-          setupButton.setAttribute('aria-expanded', 'false');
-          setupButton.focus();
-          setupStep.classList.remove('show');
-        }
-
-        // Handle arrow key navigation
-        if (setupStep.classList.contains('show')) {
-            const stepItems = setupStep.querySelectorAll('[role="menuitem"]');
-            let focusedIndex = Array.from(stepItems).indexOf(document.activeElement);
-      
-            if (event.key === 'ArrowDown' || event.key === 'ArrowRight') {
-              event.preventDefault(); // Prevent scrolling on ArrowDown
-              let nextIndex = (focusedIndex + 1) % stepItems.length;
-              stepItems[nextIndex].focus();
-            } else if (event.key === 'ArrowUp' || event.key === 'ArrowLeft') {
-              event.preventDefault(); // Prevent scrolling on ArrowUp
-              let prevIndex = (focusedIndex - 1 + stepItems.length) % stepItems.length;
-              stepItems[prevIndex].focus();
-            }
-          }
-
-
-      });
-
   
-});
-
-
+  // Usage for the bell element
+  const bell = document.querySelector(".notifyButton");
+  const bellDrop = document.querySelector(".drop");
+  const bellItems = bellDrop.querySelectorAll('[role="menuitem"]');
+  handleToggle(bell, bellDrop, bellItems, "dropOpen");
+  
+  // Usage for the info element
+  const info = document.querySelector(".storename");
+  const infoTab = document.querySelector(".storeTab");
+  const infoItems = infoTab.querySelectorAll('[role="menuitem"]');
+  handleToggle(info, infoTab, infoItems, "tabOpen");
+  
+  // Usage for the Setup element
+  const setupButton = document.getElementById('setupButton');
+  const setupStep = document.getElementById('setupStep');
+  const setupStepItems = setupStep.querySelectorAll('[role="menuitem"]');
+  handleToggle(setupButton, setupStep, setupStepItems, "show");
+  
